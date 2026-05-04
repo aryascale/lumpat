a7ef7b5ef81536baaad0e49e0a2a9cc10179f50a
Docs <arya@MacBook-Pro-2.local>
2026-05-04 13:19:18 +0700

    chore: remove scratch files

--- scratch/check_db.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const events = await prisma.event.findMany({
-    select: { id: true, name: true, bannerUrl: true }
-  });
-  console.log(JSON.stringify(events, null, 2));
-}
-main();
